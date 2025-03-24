b9c3573231d2dac38cd16d47f1eb3112b0e256fe
xiao <1710345064@qq.com>
2025-03-24 11:32:22 +0800

    message 6:Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.common.by import By
 
 # browser = webdriver.Chrome()
 # browser.get("http://localhost:8000")
-
 # assert 'Django' in browser.page_source
 # assert "To-Do" in browser.title
 # browser.quit()
@@ -16,13 +18,25 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_and_retrieve_it_later(self):
-        self.browser.get("http://localhost:8000")
-        (
-            self.assertIn("To-Do", self.browser.title),
-            "browser title was:" + self.browser.title,
-        )
-        self.fail("Finish the test!")
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME,'h1').text
+        self.assertIn('To-Do',header_text)
+
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )   
+        inputbox.send_keys('Buy flowers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID,'id_list_table')
+        rows = table.find_element(By.TAG_NAME,'tr')
+        self.assertIn('1:Buy flowers',[row.text for row in rows])
+        self.fail('Finish the test!')
 
 
 if __name__ == "__main__":
