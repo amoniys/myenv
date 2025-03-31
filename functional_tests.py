73a1c7040e7278ec892d7572ef5b7c760266db08
xiao <1710345064@qq.com>
2025-03-31 18:50:38 +0800

    message 8 :Before refactoring the functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -19,24 +19,49 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+
+        #张三听说有一个在线待办事项的应用
+        #他去看了这个应用的首页
         self.browser.get('http://localhost:8000')
+
+        #他注意到网页的标题和头部都包含“To-Do”这个词
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element(By.TAG_NAME,'h1').text
         self.assertIn('To-Do',header_text)
 
-        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        #应用有一个输入待办事项的文本输入框
+        inputbox = self.browser.find_element(By.ID,'id_new_item') #（1)
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )   
+
+        #他在文本输入框中输入了“Buy flowers”
         inputbox.send_keys('Buy flowers')
+
+        #他按了回车键，页面更新了
+        #待办事项表格中显示了“1: Buy flowers”
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element(By.ID,'id_list_table')
+        rows = table.find_elements(By.TAG_NAME,'tr')
+        self.assertIn('1: Buy flowers', [row.text for row in rows])
+
+        #界面中又显示了一个文本输入框，可以输入其他待办事项
+        #他输入了“ Give a lift to Lisi”
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        inputbox.send_keys('Give a lift to Lisi')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
+        #页面再次更新，她的清单中显示了这两个代办事项
         table = self.browser.find_element(By.ID,'id_list_table')
         rows = table.find_elements(By.TAG_NAME,'tr')
-
         self.assertIn('1: Buy flowers', [row.text for row in rows]) 
+        self.assertIn('2: Give a lift to Lisi', [row.text for row in rows])
+
+        #张三想知道这个网站是否会记住他的清单
+        #他看到王章为他生成了一个唯一的URL
         self.fail('Finish the test!')
 
 
