38b73d2c3a24aeeb7e8b37ad0942a99b3ebc148e
xiao <1710345064@qq.com>
2025-04-21 17:54:57 +0800

    message 15: new URL,view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -25,30 +25,9 @@ class ItemModelTest(TestCase):
         self.assertEqual(second_saved_item.text,'Item the second')
 
 class HomePageTest(TestCase):
-
-    def test_display_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-        response = self.client.get('/')
-        self.assertIn('itemey 1',response.content.decode())
-        self.assertIn('itemey 2',response.content.decode())
-
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response,'home.html')
-
-   #def test_can_save_a_POST_request(self):
-       # response = self.client.post('/',data={'item_text':'A new list item'})
-       
-        #self.assertEqual(Item.objects.count(),1) #(1)
-        #new_item = Item.objects.first() #(2)
-        #self.assertEqual(new_item.text,'A new list item') #(3)
-
-        #self.assertIn('A new list item',response.content.decode())
-        #self.assertTemplateUsed(response,'home.html')
-
-        #self.assertEqual(response.status_code,302)
-        #self.assertEqual(response['location'],'/')
     
     def test_can_save_a_POST_request(self):
         response = self.client.post('/',data={'item_text':'A new list item'})
@@ -61,10 +40,21 @@ class HomePageTest(TestCase):
         response = self.client.post('/',data={'item_text':'A new list item'})
         
         self.assertEqual(response.status_code,302)
-        self.assertEqual(response['location'],'/')
+        self.assertEqual(response['location'],'/lists/the-new-page/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(),0)
 
-# Create your tests here.
+class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-new-page/')
+        self.assertTemplateUsed(response,'list.html')
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-new-page/')
+
+        self.assertContains(response,'itemey 1')
+        self.assertContains(response,'itemey 2')
